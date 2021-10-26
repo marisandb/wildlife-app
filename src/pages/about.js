31c69eccc33b86a7bf3e38447489214b5da193f5
jake <jakedlee2018@gmail.com>
2021-10-25 21:00:49 -0500

    Page layouts

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+function About() {
+    return (
+        <section id='about'>
+            <div>
+                <h2>
+                    About Us
+                </h2>
+                <p>
+                    Lorum Ippsum blah blah blah.
+                </p>
+            </div>
+        </section>
+    );
+}
+
+export default About;
