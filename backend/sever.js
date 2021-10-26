b51d32576101cfdf0958d290a5b15557b3fa9340
Rugger11 <82486396+zimmermanc6588@users.noreply.github.com>
2021-10-25 21:38:14 -0500

    updating files

--- backend/sever.js
+++ backend/sever.js
@@ -1,14 +1,26 @@
 import express from 'express';
+import { connect } from 'net';
 const cors=require('cors');
 const mongoose=require('mongoose');
 
-require('dotnv').config();
+require('dotenv').config();
 
 const app=express();
 const port =process.env.Port || 5000;
 
+
 app.use(cors());
 app.use(express.json());
+
+const uri =process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true}
+);
+
+const connection =mongoose.connection;
+connection.once('open',() => {
+console.log("MongoDB database connection established successfully")
+
+});
 
 app.listen(port, () =>{
     console.log(`Server is running on port ${port}`);
